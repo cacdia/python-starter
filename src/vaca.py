0e3ecebaba3eb574a50fe93013b81f46d1f5a8df
Thiago Bezerra <thiagobezerra.dev@gmail.com>
2025-07-04 18:02:39 -0300

    refactor(main): simplifica main para usar vaca_diz e remove código de exemplo antigo

---
+++ src/vaca.py
@@ -0,0 +1,15 @@
+def vaca_diz(frase: str) -> None:
+    """
+    Exibe uma mensagem em um balão de fala acompanhado de uma arte ASCII de vaca.
+
+    Esta função imprime a mensagem fornecida seguida de uma representação visual de uma vaca em ASCII art.
+
+    Args:
+        frase (str): Mensagem a ser exibida no balão de fala.
+    """
+    print(f"  {frase}")
+    print("   \\   ^__^")
+    print("    \\  (oo)\\_______")
+    print("       (__)\\       )\\/\\")
+    print("           ||----w |")
+    print("           ||     ||")
